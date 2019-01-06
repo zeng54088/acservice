0864304f94ef947307cd7de368dc3c57b294b745
zeng54088 <617002110@qq.com>
2019-01-06 20:15:17 +0800

    '整合shrio'

--- src/main/java/com/tree/acservice/test/controller/TestController.java
+++ src/main/java/com/tree/acservice/test/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.tree.acservice.test.controller;
 
 import com.tree.acservice.common.entity.BaseVo;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,7 +18,8 @@ public class TestController {
     @ResponseBody
     public BaseVo hello(){
         BaseVo ba=new BaseVo();
-
+        Subject currentUser = SecurityUtils.getSubject();
+        System.out.println(currentUser);
         return ba;
     }
 
